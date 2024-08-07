57b4512b10b814a34b69501860317294d58bd6cc
Fernando Fernandez <frfernandezdev@gmail.com>
2024-08-07 19:52:23 -0400

    (ci/cd): update .env.ci

--- config/database.php
+++ config/database.php
@@ -112,6 +112,12 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
     ],
 
     /*
